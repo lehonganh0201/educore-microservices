e2594898f808d86e1469abbfb07eea99c8ad29bb
lehonganh0201 <le2960620@gmail.com>
2026-07-22 14:54:28 +0700

    feat: init student service

--- script/postgres/init/01-init-databases.sql
+++ script/postgres/init/01-init-databases.sql
@@ -5,3 +5,8 @@ SELECT 'CREATE DATABASE keycloak'
     WHERE NOT EXISTS (
     SELECT FROM pg_database WHERE datname = 'keycloak'
 )\gexec
+
+SELECT 'CREATE DATABASE student_db'
+    WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'student_db'
+)\gexec
